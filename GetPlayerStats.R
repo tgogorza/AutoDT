51b15cda016c56c44f5d5d0bad5520c4695fe8cb
Tomas Gogorza <tgogorza@gmail.com>
2015-07-30 21:04:19 -0300

    Latest updates
    Playing around with genethic algorithm

---
+++ GetPlayerStats.R
@@ -0,0 +1,29 @@
+GetPlayerStats <- function(){
+  library(plyr)
+  library(dplyr)
+  library(httr)
+  library(XML)
+  
+  url <- "http://www.ungrandt.com.ar/planteles/"
+  #Get teams
+  teams <- read.csv("teams.csv")
+  #Scrape teams player stats
+  stats <- lapply(teams$team,GetTeamPlayers)
+  stats <- ldply(stats, data.frame)
+  #Write stats to a csv file
+  write.csv(stats,"playerstats.csv")
+  return(stats) 
+}
+
+##Input: a team name
+##Gets a given teams players' stats
+##Output: table of players stats
+GetTeamPlayers <- function(team){
+  address <- paste(url,team,sep = "")
+  list <- readHTMLTable(address)
+  if(length(list) > 1)
+    players <- readHTMLTable(address)[[2]]
+  else
+    players <- readHTMLTable(address)[[1]]
+  players <- mutate(players,equipo = team)
+}
